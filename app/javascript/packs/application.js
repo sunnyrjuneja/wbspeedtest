aebad73c513414b5beee2e8b3e463927832e654b
Sunny Juneja <jr.sunny@gmail.com>
2018-03-13 13:34:50 -0700

    Add typescript
    * bin/rails webpacker:install:typescript
    * yarn add ts-loader@3
    * yarn add typescript
    * mv app/javascript/packs/hello_typescript.ts app/javascript/src/hello_typescript.ts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import 'src/hello_typescript'
+
 console.log('Hello World from Webpacker')
 
 import fontawesome from '@fortawesome/fontawesome'
---
+++ app/javascript/src/hello_typescript.ts
@@ -0,0 +1,8 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_typescript' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb.
+
+console.log('Hello world from typescript');
+
+class HelloTypescript {
+  key: "value";
+}
